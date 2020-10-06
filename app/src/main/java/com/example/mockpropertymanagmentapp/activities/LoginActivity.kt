2e7bb544f3e48a95bdc9c11fbcdff887b4f2d21c
Adam Reeves <48875460+irish1uck@users.noreply.github.com>
2020-10-05 21:42:55 -0700

    Basic structures in place

--- app/src/main/java/com/example/mockpropertymanagmentapp/activities/LoginActivity.kt
+++ app/src/main/java/com/example/mockpropertymanagmentapp/activities/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.example.mockpropertymanagmentapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mockpropertymanagmentapp.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,11 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun init() {
-
+        button_login_submit.setOnClickListener{
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+        button_login_to_register.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
