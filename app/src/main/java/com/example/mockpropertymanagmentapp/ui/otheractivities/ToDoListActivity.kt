b656faf80aa984993b7def64e91d86984cf596d5
Adam Reeves <48875460+irish1uck@users.noreply.github.com>
2020-10-13 09:35:29 -0700

    UI Updates to Home screen

--- app/src/main/java/com/example/mockpropertymanagmentapp/ui/otheractivities/ToDoListActivity.kt
+++ app/src/main/java/com/example/mockpropertymanagmentapp/ui/otheractivities/ToDoListActivity.kt
@@ -8,5 +8,10 @@ class ToDoListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_to_do_list)
+        init()
+    }
+
+    private fun init() {
+
     }
 }
