55bdd5d7796ff6d8e01b418db7d88f90b2f6c138
Adam Reeves <admchristopher@gmail.com>
2020-10-19 14:38:50 -0700

    Contact Us button added and function for emailing me

--- app/src/main/java/com/example/mockpropertymanagmentapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/mockpropertymanagmentapp/ui/home/HomeActivity.kt
@@ -2,19 +2,25 @@ package com.example.mockpropertymanagmentapp.ui.home
 
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import com.example.mockpropertymanagmentapp.R
 import com.example.mockpropertymanagmentapp.helpers.SessionManager
+import com.example.mockpropertymanagmentapp.helpers.toastShort
 import com.example.mockpropertymanagmentapp.ui.otheractivities.*
 import com.example.mockpropertymanagmentapp.ui.properties.activities.PropertyActivity
 import com.example.mockpropertymanagmentapp.ui.todolist.activities.ToDoListActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.app_bar.*
+import kotlinx.android.synthetic.main.contact_bottom_sheet.*
+import kotlinx.android.synthetic.main.contact_bottom_sheet.view.*
 
 class HomeActivity : AppCompatActivity() {
     lateinit var sessionManager: SessionManager
+    lateinit var bottomSheetDialog: BottomSheetDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -24,6 +30,21 @@ class HomeActivity : AppCompatActivity() {
 
     private fun init() {
         toolbar()
+        bottomSheetDialog = BottomSheetDialog(this)
+        val view = layoutInflater.inflate(R.layout.contact_bottom_sheet, null)
+        bottomSheetDialog.setContentView(view)
+        button_contact_us.setOnClickListener {
+            bottomSheetDialog.show()
+        }
+        view.button_contact_send_email.setOnClickListener {
+            val subject = view.edit_text_contact_subject.text.toString().trim()
+            val message = view.edit_text_contact_message.text.toString().trim()
+            val recipient = "adam@adamrdev.com".trim()
+            if(subject != "" && message != ""){
+                sendEmail(recipient, subject, message)
+                bottomSheetDialog.dismiss()
+            } else this.toastShort("You have to fill out both fields")
+        }
         button_home_logout.setOnClickListener {
             dialogLogout()
         }
@@ -77,4 +98,20 @@ class HomeActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
     }
 
+    private fun sendEmail(recipient: String, subject: String, message: String) {
+        val mIntent = Intent(Intent.ACTION_SEND)
+        mIntent.data = Uri.parse("mailto:")
+        mIntent.type = "text/plain"
+        mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+        mIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        mIntent.putExtra(Intent.EXTRA_TEXT, message)
+
+        try {
+            startActivity(Intent.createChooser(mIntent, "Choose Email Client..."))
+        }
+        catch (e: Exception){
+            e.message?.let { this.toastShort(it) }
+        }
+
+    }
 }
